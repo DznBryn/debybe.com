49763e29906902ebe93f4aa596eadb652d04ff57
BrianDemorcy <501526@containerstore.com>
2025-02-26 20:07:56 -0500

    Fixed mobile landing page

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -8,7 +8,7 @@ interface AnimatedTextProps {
 const AnimatedText: React.FC<AnimatedTextProps> = ({ content }) => {
 	return (
 		<div className='flex flex-col gap-4 p-2'>
-      <h1 className='text-6xl font-bold animate-pulse text-red-950'>
+      <h1 className='text-6xl font-bold animate-pulse text-red-950 max-w-72 sm:max-w-full'>
 			{content.split('').map((char, index) => (
 				<span key={index} className='char' style={{ animationDelay: `${index * 100}ms` }}>
 					{char === ' ' ? '\u00A0' : char}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavItem from './NavItem';
 
 const Navbar: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <nav className="text-white p-4 w-full max-w-xl">
-            <ul className="flex justify-between space-x-4 w-full">
-                <NavItem href="/apps/todo">Projects</NavItem>
-                <NavItem href="/about">About</NavItem>
-                <NavItem href="/services">Services</NavItem>
-                <NavItem href="/contact">Contact</NavItem>
-            </ul>
-        </nav>
-    );
+			<nav className='text-white p-4 w-full max-w-xl'>
+				<div className='flex items-center w-full justify-center'>
+					<button onClick={toggleMenu} className='md:hidden'>
+						<div className='w-8 h-1 border rounded-full bg-red-950 mb-1'></div>
+						<div className='w-8 h-1 border rounded-full bg-red-950 mb-1'></div>
+						<div className='w-8 h-1 border rounded-full bg-red-950'></div>
+					</button>
+					<ul
+						className={`hidden justify-between space-x-4 w-full md:flex md:space-x-4 `}>
+						<NavItem href='/apps/todo'>Projects</NavItem>
+						<NavItem href='/about'>About</NavItem>
+						<NavItem href='/services'>Services</NavItem>
+						<NavItem href='/contact'>Contact</NavItem>
+					</ul>
+				</div>
+
+				{isOpen && (
+					<div className='fixed inset-0 bg-white bg-opacity-95 flex flex-col items-center justify-center z-50'>
+						<button
+							onClick={toggleMenu}
+							className='absolute top-4 right-4 text-red-950 text-2xl'>
+							X
+						</button>
+						<ul className='flex flex-col justify-center items-center space-y-16'>
+							<NavItem href='/apps/todo'>Projects</NavItem>
+							<NavItem href='/about'>About</NavItem>
+							<NavItem href='/services'>Services</NavItem>
+							<NavItem href='/contact'>Contact</NavItem>
+						</ul>
+					</div>
+				)}
+			</nav>
+		);
 };
 
 export default Navbar; 
